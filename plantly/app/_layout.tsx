cb3c064eab484f1346e4af29a1f74df487dec248
manoloramon <manolo.manatlao@gmail.com>
2025-08-06 10:39:51 +0400

    Full-Screen Modals

--- plantly/app/_layout.tsx
+++ plantly/app/_layout.tsx
@@ -1,10 +1,12 @@
 import { Stack } from "expo-router";
 
+
 export default function Layout() {
     return (
         <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false, animation: "fade" }} />
             <Stack.Screen name="onboarding" options={{ headerShown: false, animation: "fade" }} />
+            <Stack.Screen name="new" options={{ presentation: "modal", title:"New Plant" }} />
         </Stack>
     )
 }
